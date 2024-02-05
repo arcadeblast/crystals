3b9550d91873ea4d19a225757717c5e47b79c310
arcadeblast <tfraking@gmail.com>
2024-02-05 00:17:41 -0500

    Add ability to attack foes

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
-let x = 0;
+function updateState() {
+}
 
-setInterval(function(){
-  x++;
-  document.getElementById("x").innerHTML = x;
-}, 1000);
+function updateHTML() {
+  document.getElementById("curr_layer").innerHTML = curr_layer;
+  document.getElementById("curr_zone").innerHTML = curr_zone;
+  document.getElementById("num_foes").innerHTML = num_foes;
+  let foe_divs = document.getElementsByClassName("foe");
+  for(let i = 0; i < foe_divs.length; i++) {
+    foe_divs[i].innerHTML = "foe" + i + " " + foes[i]
+  }
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+setInterval(() => {
+  updateHTML();
+  updateState();
+}, 1);
+
+let curr_layer = 1;
+let curr_zone = 1;
+let num_foes = 5;
+
+var foes = new Array(num_foes).fill(100);
+
+foes_div = document.getElementById("foes")
+for(let i = 0; i < foes.length; i++) {
+  const foe_div = document.createElement("div");
+  foe_div.classList.add("foe")
+  foes_div.appendChild(foe_div);
+}
+
+attack_button = document.getElementById("attack");
+attack_button.addEventListener('click', ()=> {
+  foes[getRandomInt(foes.length)] -= 20;
+})
