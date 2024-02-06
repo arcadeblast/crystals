42d1935d0391314de8bb84a533e37583f9c75041
arcadeblast <tfraking@gmail.com>
2024-02-05 22:00:16 -0500

    Remove enemies from the board when they reach negative hp

--- app.js
+++ app.js
@@ -4,15 +4,32 @@ function updateState() {
 function updateHTML() {
   document.getElementById("curr_layer").innerHTML = curr_layer;
   document.getElementById("curr_zone").innerHTML = curr_zone;
-  document.getElementById("num_foes").innerHTML = num_foes;
-  let foe_divs = document.getElementsByClassName("foe");
-  for(let i = 0; i < foe_divs.length; i++) {
-    foe_divs[i].innerHTML = "foe" + i + " " + foes[i]
+  document.getElementById("num_foes").innerHTML = foes.length;
+  document.getElementById("coins").innerHTML = coins;
+  let foes_div = document.createElement("div");
+  for(let i = 0; i < foes.length; i++) {
+    let foe_div = document.createElement("div");
+    foe_div.classList.add("foe");
+    foe_div.innerHTML = foes[i].name + i + " " + foes[i].hp;
+    foes_div.appendChild(foe_div);
   }
+  document.getElementById("foes").replaceWith(foes_div);
+  foes_div.id = "foes";
 }
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
+}
+
+function attack_random(foes) {
+  let attack_strength = getRandomInt(10) + 10;
+  let foe_i = getRandomInt(foes.length);
+  let foe = foes[foe_i]
+  foe.hp -= attack_strength;
+  if(foe.hp <= 0) {
+    coins += 1;
+    foes.splice(foe_i, 1);
+  }
 }
 
 setInterval(() => {
@@ -23,17 +40,23 @@ setInterval(() => {
 let curr_layer = 1;
 let curr_zone = 1;
 let num_foes = 5;
+let coins = 0;
 
-var foes = new Array(num_foes).fill(100);
+let foes = []
 
-foes_div = document.getElementById("foes")
-for(let i = 0; i < foes.length; i++) {
-  const foe_div = document.createElement("div");
-  foe_div.classList.add("foe")
-  foes_div.appendChild(foe_div);
+for(let i = 0; i < num_foes; i++) {
+  foes.push({
+    name: "Goblin",
+    hp: 100
+  })
 }
+
+foes.push({
+  name: "Supergoblin",
+  hp: 500
+})
 
 attack_button = document.getElementById("attack");
 attack_button.addEventListener('click', ()=> {
-  foes[getRandomInt(foes.length)] -= 20;
+  attack_random(foes);
 })
